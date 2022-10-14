e50833c3024afb4b76c3d8693806432104721199
Pavithrahc <pavithramubbu93@gmail.com>
2022-10-14 07:01:23 +0530

    Added UC1 Creating PlayRoleService

--- src/main/java/com/bridgelabz/EmployeePayRoleData.java
+++ src/main/java/com/bridgelabz/EmployeePayRoleData.java
@@ -1,4 +1,21 @@
 package com.bridgelabz;
 
 public class EmployeePayRoleData {
+    int employeeId;
+    String employeeName;
+    double employeeSalary;
+
+    public EmployeePayRoleData(Integer id, String name, Double salary) {
+
+        this.employeeId = id;
+        this.employeeName = name;
+        this.employeeSalary = salary;
+    }
+
+    @Override
+    public String toString() {
+
+        return "Employee Details: \nEmployee Id: "+employeeId+"\nEmployee Name: "+employeeName+"\nEmployee Salary: "+employeeSalary;
+    }
 }
+
--- src/main/java/com/bridgelabz/EmployeePayRoleService.java
+++ src/main/java/com/bridgelabz/EmployeePayRoleService.java
@@ -1,7 +1,48 @@
 package com.bridgelabz;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 public class EmployeePayRoleService {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Employee PayRole Service");
+    public enum IOService {
+        CONSOLE_IO, FILE_IO, DB_IO, REST_IO
     }
+
+    private List<EmployeePayRoleData> employeePayrollList;
+
+    public EmployeePayRoleService(ArrayList<EmployeePayRoleService> employeePayrollList) {
+    }
+
+    public EmployeePayRoleService(List<EmployeePayRoleData> employeePayrollList) {
+        this.employeePayrollList = employeePayrollList;
+    }
+
+    private void readEmployeePayrollData(Scanner consoleInputReader) {
+
+        System.out.println("Enter the Employee Id : ");
+        int id = consoleInputReader.nextInt();
+        System.out.println("Enter the Employee Name : ");
+        String name = consoleInputReader.next();
+        System.out.println("Enter the Employee Salary : ");
+        double salary = consoleInputReader.nextDouble();
+
+        employeePayrollList.add(new EmployeePayRoleData(id, name, salary));
+    }
+
+    private void writeEmployeePayrollData() {
+        System.out.println("\nWriting Employee Payroll Roster to Console\n" + employeePayrollList);
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("---------- Welcome To Employee Payroll Application ----------\n");
+        ArrayList<EmployeePayRoleService> employeePayrollList  = new ArrayList<EmployeePayRoleService>();
+        EmployeePayRoleService employeePayrollService = new EmployeePayRoleService(employeePayrollList);
+        Scanner consoleInputReader = new Scanner(System.in);
+
+        employeePayrollService.readEmployeePayrollData(consoleInputReader);
+        employeePayrollService.writeEmployeePayrollData();
+
+    }
+
 }
